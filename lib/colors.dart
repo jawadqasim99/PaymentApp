ba864feb6f3477c60f8677414e0a15f187e09a31
Jawad Qasim <jawadqasim99@gmail.com>
2023-09-20 20:10:47 +0500

    Now,we are starding Payment app with new skills

---
+++ lib/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backGroundColors = Color(0xFFecf1f4);
+  static const Color mainColor = Color(0xFF192c49);
+  static const Color green = Color(0xFF73be93);
+  static const Color idColor = Color(0xFF9ca2ac);
+  static const Color selectBackground = Color(0xFFf1f4f8);
+  static const Color selectColor = Color(0xFF6f7c8b);
+  static const Color halfOval = Color(0xFF646f80);
+  static const Color cancelColor = Color(0xFF3c4b60);
+}
