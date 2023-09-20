5eb882005d9e97a2c94faad1d30f45ab9f149420
Jawad Qasim <jawadqasim99@gmail.com>
2023-09-20 21:56:05 +0500

    we complete only head section

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paymentapp/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,8 +34,78 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(),
+      backgroundColor: AppColors.backGroundColors,
+      body: Container(
+        height: h,
+        child: Stack(
+          children: [_headsection(), _listview(), _listview()],
+        ),
+      ),
     );
+  }
+
+  _headsection() {
+    return Container(
+      height: 310,
+      child: Stack(
+        children: [
+          _mainBackground(),
+          _buttonClick(),
+        ],
+      ),
+    );
+  }
+
+  _mainBackground() {
+    return Positioned(
+        child: Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.fitHeight,
+              image: AssetImage("assets/images/back.jpg"))),
+    ));
+  }
+
+  _buttonClick() {
+    return Positioned(
+        bottom: 12,
+        right: 50,
+        child: Container(
+          child: CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.black,
+            child: IconButton(
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ));
+  }
+
+  _listview() {
+    return Positioned(
+        top: 330,
+        child: Container(
+          height: 110,
+          width: MediaQuery.of(context).size.width - 20,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xFFd8dbe0),
+                    offset: Offset(1, 1),
+                    blurRadius: 20.0,
+                    spreadRadius: 10.0)
+              ],
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  bottomRight: Radius.circular(30))),
+        ));
   }
 }
